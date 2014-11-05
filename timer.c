883d02cb176c75ee416c16a1a76ad80cd2d3e283
Dajo Hofman <dhofman@gmx.com>
2014-11-05 10:38:27 +0100

    Added project MPLAB X C project

---
+++ timer.c
@@ -0,0 +1,11 @@
+#if defined(__XC)
+    #include <xc.h>         /* XC8 General Include File */
+#elif defined(HI_TECH_C)
+    #include <htc.h>        /* HiTech General Include File */
+#endif
+
+#include "timer.h"
+
+void timerInit(void) {
+    T1CON = 0b00000001;
+}
---
+++ timer.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   timer.h
+ * Author: Dodedodo33
+ *
+ * Created on November 4, 2014, 10:48 PM
+ */
+
+#ifndef TIMER_H
+#define	TIMER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void timerInit(void);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TIMER_H */
+
